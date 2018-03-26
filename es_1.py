397094030db5d74d65a856129fc6d80840e5db0a
sordinho <s228457@studenti.polito.it>
2018-03-26 14:39:26 +0200

    Esercizio 1, introdotto sort prima della stampa

---
+++ es_1.py
@@ -0,0 +1,22 @@
+# To Do manager Lab 2 with sorted print
+
+def printMenu(): #Funzione per la stampa del menu
+    print("------------------------------------------")
+    print("1) Insert a new task")
+    print("2) Remove a task")
+    print("3) Show tasks")
+    print("4) Exit")
+
+tasks = list()
+
+while cmd != '4':
+    printMenu()
+    cmd = input("Inserire il comando: ")
+    if cmd == '1':
+        task = input("Inserisci un task: ")
+        tasks.append(task)
+    elif cmd == '2':
+        target = input("Inserisci un task da rimuovere: ")
+        tasks.remove(target)
+    elif cmd == '3':
+        print(sorted(tasks))
